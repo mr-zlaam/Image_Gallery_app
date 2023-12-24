0e357a7da88f3baddc828619d943a5e09e9f6c0a
Zlaam <mrzalaam@gmail.com>
2023-12-24 11:54:37 +0500

    setting theme on local storage to prevent auto change

--- src/3Components/1Navbar/jsx/ModeSwitcher.jsx
+++ src/3Components/1Navbar/jsx/ModeSwitcher.jsx
@@ -1,28 +1,46 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { BsFillMoonFill, BsSunFill } from "react-icons/bs";
 import { ThemeContext } from "../../../1Context/Context";
+
 const ModeSwitcher = () => {
   const { isDarkMode, setIsDarkMode } = useContext(ThemeContext);
 
-  const DarkMode = () => {
-    setIsDarkMode(false);
+  useEffect(() => {
+    const storedDarkMode = localStorage.getItem("isDarkMode");
+    setIsDarkMode(storedDarkMode === true);
+  }, [setIsDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevDarkMode) => {
+      const newDarkMode = !prevDarkMode;
+      localStorage.setItem("isDarkMode", newDarkMode);
+      return newDarkMode;
+    });
   };
-  const LightMode = () => {
-    setIsDarkMode(true);
+
+  const toggleLightMode = () => {
+    setIsDarkMode((prevDarkMode) => {
+      const newDarkMode = !prevDarkMode;
+      localStorage.setItem("isDarkMode", newDarkMode);
+      return newDarkMode;
+    });
   };
 
   return (
-    <>
+    <div className="theme_toggler">
       {isDarkMode ? (
-        <div onClick={DarkMode} className="theme_toggler">
-          <BsSunFill className="icon_sun cursor-pointer toggler_icon  " />
-        </div>
+        <BsSunFill
+          onClick={toggleLightMode}
+          className="icon_sun cursor-pointer toggler_icon"
+        />
       ) : (
-        <div onClick={LightMode} className="theme_toggler">
-          <BsFillMoonFill className="icon_moon cursor-pointer toggler_icon  " />
-        </div>
+        <BsFillMoonFill
+          onClick={toggleDarkMode}
+          className="icon_moon cursor-pointer toggler_icon"
+        />
       )}
-    </>
+    </div>
   );
 };
+
 export default ModeSwitcher;
